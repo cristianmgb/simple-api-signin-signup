8bc59d639440389bf1d419d3b41853186fff8748
Cristian Gonzalez <cristian@bellpi.com>
2021-11-23 14:14:53 -0500

    fix signin

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,24 +6,23 @@ async function signup(req, res) {
   const { email, password, name, cellphone } = req.body;
   if (auths) {
     const result = auths.find((auth) => auth.email === email);
-      if (result) {
-        res
-          .status(400)
-          .send({ status: "ERROR", message: "Email already exists" });
-      } else {
-        const id = uuidv4();
-        const newAuth = [...auths];
-        newAuth.push({ id, email, password, name, cellphone });
+    if (result) {
+      res
+        .status(400)
+        .send({ status: "ERROR", message: "Email already exists" });
+    } else {
+      const id = uuidv4();
+      const newAuth = [...auths];
+      newAuth.push({ id, email, password, name, cellphone });
 
-        storeData(newAuth);
+      storeData(newAuth);
 
-        res.status(200).send({
-          data: { id, email, name, cellphone },
-          message: "User created successfully",
-          status: "OK",
-        });
-      }
-    
+      res.status(200).send({
+        data: { id, email, name, cellphone },
+        message: "User created successfully",
+        status: "OK",
+      });
+    }
   } else {
     const id = uuidv4();
     const newAuth = [];
@@ -42,12 +41,11 @@ async function signup(req, res) {
 async function signin(req, res) {
   const auths = getData();
 
-  const result = auths.find(
-    (auth) =>
-      auth.email === req.body.email && auth.password === req.body.password
-  );
-
   if (auths) {
+    const result = auths.find(
+      (auth) =>
+        auth.email === req.body.email && auth.password === req.body.password
+    );
     if (result) {
       res.status(200).send({
         data: {
--- src/database/db.js
+++ src/database/db.js
@@ -1,26 +1,3 @@
-// import { join, dirname } from "path";
-// import { Low, JSONFile } from "lowdb";
-// import { fileURLToPath } from "url";
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// // Use JSON file for storage
-// const file = join(__dirname, "db.json");
-// const adapter = new JSONFile(file);
-// const db = new Low(adapter);
-
-// // Read data from JSON file, this will set db.data content
-// await db.read();
-
-// // If file.json doesn't exist, db.data will be null
-// // Set default data
-// db.data = {};
-// if (!db.data.hasOwnProperty("auths")) {
-//   db.data = { auths: [] };
-// }
-
-// export default db;
-
 import { LocalStorage } from "node-localstorage";
 
 const sotre = new LocalStorage("./scratch");
